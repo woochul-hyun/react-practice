30fa3cb1ef54ce84d28b218ad66e55771dd0ef5f
Woochul Hyun <woochul.hyun91@gmail.com>
2019-09-14 11:06:27 +0900

    init

---
+++ react-redux-example/src/modules/counter.js
@@ -0,0 +1,21 @@
+import { createAction, handleActions } from 'redux-actions';
+
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
+
+const initState = {
+  number: 0
+};
+
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 })
+  },
+  initState
+);
+
+export default counter;
